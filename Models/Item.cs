ccfd17ec7f7d24369cce1fc7cc456fef8964b5cd
Swathi Js <Swathi_Js@gep.com>
2021-06-30 21:30:45 +0530

    Add project files.

---
+++ Models/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace todo.Models
+{
+    using Newtonsoft.Json;
+
+    public class Item
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "city")]
+        public string City { get; set; }
+
+        [JsonProperty(PropertyName = "state")]
+        public string State { get; set; }
+
+        [JsonProperty(PropertyName = "country")]
+        public string Country { get; set; }
+
+        [JsonProperty(PropertyName = "Zip")]
+        public string Zip { get; set; }
+    }
+}
